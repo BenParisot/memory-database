56f555a684389659eb5d4ec5c48a0ad4a5c30085
Ben Parisot <me@benparisot.com>
2019-04-02 16:01:31 -0700

    create uid object in memory database

--- tests/Memory.test.js
+++ tests/Memory.test.js
@@ -1,8 +1,11 @@
 const MemoryDatabase = require('../lib/Memory');
+const memory = new MemoryDatabase();
 
 describe('Memory Database', () => {
   it('creates an empty store object', () => {
-    const memory = new MemoryDatabase();
     expect(memory.store).toEqual({});
+  });
+  it('creates a unique identifier for the object', () => {
+    expect(memory.create()).toEqual(expect.any(String));
   });
 });
