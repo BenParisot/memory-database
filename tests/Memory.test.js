770a3c0dbdbf565a2639e9be389aa00a2513460a
Ben Parisot <me@benparisot.com>
2019-04-02 16:32:34 -0700

    tested copied object is working

--- tests/Memory.test.js
+++ tests/Memory.test.js
@@ -8,4 +8,30 @@ describe('Memory Database', () => {
   it('creates a unique identifier for the object', () => {
     expect(memory.create()._id).toEqual(expect.any(String));
   });
+  it('creates a unique identifier for the object', () => {
+    const object = 
+    {
+      name:'Bob',
+      title:'president'
+    };
+    const expected = {
+      name:'Bob',
+      title:'president',
+      _id: expect.any(String)
+    };
+    expect(memory.create(object)).toEqual(expected);
+  });
+  it('creates a unique identifier for the object', () => {
+    const object = 
+    {
+      name:'Vanessa',
+      title:'ceo'
+    };
+    const expected = {
+      name:'Vanessa',
+      title:'ceo',
+      _id: expect.any(String)
+    };
+    expect(memory.create(object)).toEqual(expected);
+  });
 });
